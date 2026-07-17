1b5f0d91072a8257a1480890f72796cd6aca1e8f
Vindhya Kapoor <kapoorvindhya26@gmail.com>
2026-07-17 23:22:05 +0530

    Protect resume and job match routes using JWT middleware

--- server/routes/jobMatchRoutes.js
+++ server/routes/jobMatchRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { saveJobMatch } from "../controllers/jobMatchController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", saveJobMatch);
+router.post("/", protect, saveJobMatch);
 
 export default router;
--- server/routes/resumeRoutes.js
+++ server/routes/resumeRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { saveResumeAnalysis } from "../controllers/resumeController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", saveResumeAnalysis);
+router.post("/", protect, saveResumeAnalysis);
 
 export default router;
