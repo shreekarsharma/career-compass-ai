34ac3e74d8578af400a3ed48f56be5a08716cd25
Shreekar Sharma <shreekarsharma@Shreekars-MacBook-Pro.local>
2026-07-29 00:34:13 +0530

    final version

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "career-compass-ai-rho.vercel.app"],
+    origin: ["http://localhost:5173", "https://career-compass-ai-rho.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
